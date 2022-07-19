4fade64359b313cde05b4030e87da6c4711fb7a7
Aniket Wakchaure <aniket.wakchaure@morningstar.com>
2022-07-19 15:25:36 +0530

    add my new project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <h2>User Registration</h2>
+  <p>First Name:</p>
+  <input v-model="user.firstName" placeholder="" />
+  <p>Last Name:</p>
+  <input v-model="user.lastName" placeholder="" />
+  <button @click="addUser">Add User</button>
+  <h3>Your Fullname is: {{fullName}}</h3>
+  <table style="width:50%">
+    <thead>
+      <th>Firstname</th>
+      <th>Lastname</th>
+    </thead>
+    <tbody>
+      <tr v-for="(user,index) in userList" :key="index">
+        <td>{{user.firstName}}</td>
+        <td>{{user.lastName}}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</template>
+
+<script>
+export default{
+  name: 'HelloWorld',
+  data () {
+    return {
+      user: {
+        firstName: '',
+        lastName: ''
+      },
+      userList: []
+    }
+  },
+  methods: {
+    addUser () {
+      if (this.user.firstName !== '' && this.user.lastName !== '') {
+        this.userList.push(this.user)
+        this.user = {
+          firstName: '',
+          lastName: ''
+        }
+      } else {
+        alert('Name must be filled')
+      }
+    }
+  },
+  computed: {
+    fullName () {
+      return this.user.firstName + ' ' + this.user.lastName
+    }
+  }
+}
+</script>
+<style>
+</style>
